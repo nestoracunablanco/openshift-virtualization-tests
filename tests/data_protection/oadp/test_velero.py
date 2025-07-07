23dbbf5cd3704717f4e98b3be86295cf4ce2d968
Nestor Acuna Blanco <nestor.acuna@ibm.com>
2025-07-07 13:12:18 +0200

    test: enable velero test for s390x
    Additionaly, this change adds wait_for_running_vm where needed.
    
    Signed-off-by: Nestor Acuna Blanco <nestor.acuna@ibm.com>

--- tests/data_protection/oadp/test_velero.py
+++ tests/data_protection/oadp/test_velero.py
@@ -3,10 +3,12 @@ from ocp_resources.datavolume import DataVolume
 
 from tests.data_protection.oadp.utils import check_file_in_vm, wait_for_restored_dv
 from utilities.constants import TIMEOUT_10SEC, Images
+from utilities.virt import wait_for_running_vm
 
 pytestmark = pytest.mark.usefixtures("skip_if_no_storage_class_for_snapshot")
 
 
+@pytest.mark.s390x
 @pytest.mark.parametrize(
     "velero_backup_single_namespace",
     [
@@ -26,6 +28,7 @@ def test_backup_while_dv_create(
     velero_backup_single_namespace.wait_for_status(status="PartiallyFailed")
 
 
+@pytest.mark.s390x
 @pytest.mark.parametrize(
     "rhel_vm_with_data_volume_template",
     [
@@ -51,9 +54,11 @@ def test_restore_multiple_namespaces(
         timeout=TIMEOUT_10SEC,
         stop_status=DataVolume.Status.IMPORT_IN_PROGRESS,
     )
+    wait_for_running_vm(vm=rhel_vm_with_data_volume_template)
     check_file_in_vm(vm=rhel_vm_with_data_volume_template)
 
 
+@pytest.mark.s390x
 @pytest.mark.parametrize(
     "rhel_vm_with_data_volume_template",
     [
@@ -81,14 +86,18 @@ def test_restore_multiple_namespaces(
 def test_backup_vm_data_volume_template_with_datamover(
     rhel_vm_with_data_volume_template, velero_restore_first_namespace_with_datamover
 ):
+    wait_for_running_vm(vm=rhel_vm_with_data_volume_template)
     check_file_in_vm(vm=rhel_vm_with_data_volume_template)
 
 
+@pytest.mark.s390x
 @pytest.mark.polarion("CNV-10589")
 def test_restore_vm_with_existing_dv(rhel_vm_from_existing_dv, velero_restore_second_namespace_with_datamover):
+    wait_for_running_vm(vm=rhel_vm_from_existing_dv)
     check_file_in_vm(vm=rhel_vm_from_existing_dv)
 
 
+@pytest.mark.s390x
 @pytest.mark.polarion("CNV-10590")
 def test_restore_cloned_dv(
     cloned_rhel_dv,
@@ -97,6 +106,7 @@ def test_restore_cloned_dv(
     wait_for_restored_dv(dv=cloned_rhel_dv)
 
 
+@pytest.mark.s390x
 @pytest.mark.polarion("CNV-10591")
 def test_restore_uploaded_dv(
     uploaded_rhel_dv,
